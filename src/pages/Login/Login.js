2646f2b8cad83351b0b6d66ab6b2efacfbd9bdc5
吕晓亮 <1480418586@qq.com>
2022-12-08 21:45:06 +0800

    formik组件实现表单的相关功能

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -1,26 +1,37 @@
 import styles from './index.module.scss'
 import TopBar from "@/components/common/TopBar";
 import Input from '@/components/common/Input'
+import { useFormik } from 'formik'
 export default  function Login() {
+        const formik = useFormik({
+            initialValues:{
+                mobile:'18836587032',
+                code:'1234'
+            },
+            onSubmit:(values)=>{
+                console.log(values)
+            }
+        });
     const onExtraClick = ()=>{
         console.log('haha')
     }
+    const {values:{mobile,code},handleChange,handleSubmit} = formik;
     return (
         <div className={styles.root}>
             <TopBar>登录</TopBar>
             <div className='content'>
                 <h3>短信登录</h3>
-                <form>
+                <form onSubmit={handleSubmit}>
                     <div className='input-item'>
-                        <Input placeholder='请输入手机号'></Input>
-                        <div className='validate'>手机号码错误信息</div>
+                        <Input placeholder='请输入手机号' name='mobile' onChange={handleChange} value={mobile} autoComplete="off"></Input>
+                        {/*<div className='validate'>手机号码错误信息</div>*/}
                     </div>
                     <div className='input-item'>
-                        <Input placeholder='请输入验证码' extra='获取验证码' onExtraClick={onExtraClick}></Input>
-                        <div className='validate'>验证码错误信息</div>
+                        <Input placeholder='请输入验证码' extra='获取验证码' onExtraClick={onExtraClick} onChange={handleChange} autoComplete="off" value={code} name="code"></Input>
+                        {/*<div className='validate'>验证码错误信息</div>*/}
                     </div>
+                    <button type='submit' className='login-btn'>登录</button>
                 </form>
-                <button type='submit' className='login-btn'>登录</button>
             </div>
         </div>
 
