0379dd42f53e036aa6c9f503eebe94b3a9554f7a
吕晓亮 <1480418586@qq.com>
2022-12-26 21:55:07 +0800

    formik组件实现表单的验证和登录按钮的禁用功能

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -2,20 +2,26 @@ import styles from './index.module.scss'
 import TopBar from "@/components/common/TopBar";
 import Input from '@/components/common/Input'
 import { useFormik } from 'formik'
+import classNames from "classnames";
+import * as Yup from 'yup'
 export default  function Login() {
         const formik = useFormik({
             initialValues:{
-                mobile:'18836587032',
-                code:'1234'
+                mobile:'',
+                code:''
             },
             onSubmit:(values)=>{
                 console.log(values)
-            }
+            },
+            validationSchema:Yup.object({
+                mobile:Yup.string().required('手机号不能为空').matches(/^1[3-9]\d{9}$/,'手机号格式错误'),
+                code:Yup.string().required('验证码不能为空').matches(/^\d{6}$/,'验证码格式错误')
+            })
         });
     const onExtraClick = ()=>{
         console.log('haha')
     }
-    const {values:{mobile,code},handleChange,handleSubmit} = formik;
+    const {values:{mobile,code},handleChange,handleSubmit,errors,handleBlur,touched ,isValid} = formik;
     return (
         <div className={styles.root}>
             <TopBar>登录</TopBar>
@@ -23,14 +29,31 @@ export default  function Login() {
                 <h3>短信登录</h3>
                 <form onSubmit={handleSubmit}>
                     <div className='input-item'>
-                        <Input placeholder='请输入手机号' name='mobile' onChange={handleChange} value={mobile} autoComplete="off"></Input>
+                        <Input
+                            placeholder='请输入手机号'
+                            name='mobile'
+                            onChange={handleChange}
+                            value={mobile}
+                            autoComplete="off"
+                            onBlur = {handleBlur}
+                        ></Input>
                         {/*<div className='validate'>手机号码错误信息</div>*/}
+                        {touched.mobile && errors.mobile?<div className='validate'>{errors.mobile}</div>:null}
                     </div>
                     <div className='input-item'>
-                        <Input placeholder='请输入验证码' extra='获取验证码' onExtraClick={onExtraClick} onChange={handleChange} autoComplete="off" value={code} name="code"></Input>
+                        <Input
+                            placeholder='请输入验证码'
+                            extra='获取验证码'
+                            onExtraClick={onExtraClick}
+                            onChange={handleChange}
+                            autoComplete="off"
+                            value={code} name="code"
+                            onBlur={handleBlur}
+                        ></Input>
                         {/*<div className='validate'>验证码错误信息</div>*/}
+                        { touched.code && errors.code ?<div className='validate'>{errors.code}</div>:null}
                     </div>
-                    <button type='submit' className='login-btn'>登录</button>
+                    <button type='submit' className={classNames('login-btn',{disabled:!isValid})} disabled={!isValid}>登录</button>
                 </form>
             </div>
         </div>
